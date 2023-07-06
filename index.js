785f8810bbff78d732204790bf46275b4560c135
Habubinator <61784574+Habubinator@users.noreply.github.com>
2023-07-06 21:18:09 +0300

    Добавление возможностей, Новые баги
    Удаление повторок в базе данных при запуске, добавлены команд, обновление классов, неудачная попытка починить баги

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '6271769906:AAHZpJDpkWpxnxWpi8PohIZp66ZZ-1AcAxk';
 const bot = new TelegramBot(token, {polling: true});
 bot.setMyCommands([{ command: '/start', description: 'Найти чат' },
-                   { command: '/next', description: 'Завершить этот чат и найти следующий (НЕ РАБОТАЕТ)' }])
+                   { command: '/next', description: 'Найти следующий диалог' },
+                   { command: '/stop', description: 'Завершить этот диалог' }])
 const {initializeApp, cert} = require("firebase-admin/app")
 const {getFirestore} = require("firebase-admin/firestore")
 const serviceAccount = require('./triadFirebaseKey.json')
@@ -10,67 +11,96 @@ const serviceAccount = require('./triadFirebaseKey.json')
 // Имплементация очереди, для оптимизации поиска
 
 class Queue {
+
   constructor() {
     this.elements = {};
     this.head = 0;
     this.tail = 0;
   }
+
   enqueue(element) {
     this.elements[this.tail] = element;
     this.tail++;
   }
+
   dequeue() {
     const item = this.elements[this.head];
     delete this.elements[this.head];
     this.head++;
     return item;
   }
+
   peek() {
     return this.elements[this.head];
   }
+
   get length() {
     return this.tail - this.head;
   }
+
   get isEmpty() {
     return this.length === 0;
+  }
+
+  remove(element) {
+    let removed = 0;
+    for (let i = this.head; i < this.tail; i++) {
+      if (this.elements[i] === element) {
+        delete this.elements[i];
+        removed++;
+      } else {
+        this.elements[i - removed] = this.elements[i];
+      }
+    }
+    this.tail -= removed;
+  }
+
+  exit(index) {
+    delete this.elements[index];
   }
 }
 
 // Классы очереди и пользователя в очереди с заделом на будущее
 
-class UserQueue{
-  constructor(){
-    this.queue = new Queue()
+class UserQueue {
+  constructor() {
+    this.queue = new Queue();
   }
 
-  add(id){
+  add(id) {
     this.queue.enqueue(new WaitUser(id));
   }
 
-  find(){
-    let arrayOfFoundPeople = [this.queue.dequeue(),
-                              this.queue.dequeue(),
-                              this.queue.dequeue()]
+  find() {
+    let arrayOfFoundPeople = [
+      this.queue.dequeue(),
+      this.queue.dequeue(),
+      this.queue.dequeue()
+    ];
     return arrayOfFoundPeople;
   }
-  
-  checkIfCouldBeInitialized(){
-    if(this.queue.length >=3){
+
+  exit(userId) {
+  for (let i = this.queue.head; i < this.queue.tail; i++) {
+    if (this.queue.elements[i].id === userId) {
+      delete this.queue.elements[i]
+      break;
+    }
+  }
+}
+
+  checkIfCouldBeInitialized() {
+    if (this.queue.length >= 3) {
       return true;
     }
     return false;
   }
 }
 
+
 class WaitUser{
   constructor(id){
     this.id = id;
-  }
-}
-
-class Chat{
-  constructor(listOfPeople){
-    this.listOfPeople = listOfPeople
   }
 }
 
@@ -97,10 +127,17 @@ bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const userId = msg.from.id;
     await createUser(userId);
+
     switch (msg.text) {
       case '/start':
-        bot.sendMessage(chatId, 'Начинаю поиск...');
-        userQueue.add(userId);
+        startSearch(userId);
+        break;
+      case '/stop':
+        stopSearchOrDialog(userId);
+        break;
+      case "/next":
+        await stopSearchOrDialog(userId);
+        startSearch(userId);
         break;
     }
     run();
@@ -127,6 +164,64 @@ async function createUser(id) {
   }
 }
 
+// Костыльная функция чтобы убрать всех пользователей-дубликатов после запуска
+
+async function removeDuplicateUsers() {
+  try {
+    const usersSnapshot = await usersDb.get();
+    const usersMap = new Map();
+
+    usersSnapshot.forEach((userDoc) => {
+      const userId = userDoc.data().id;
+      if (usersMap.has(userId)) {
+        usersMap.get(userId).push(userDoc.id);
+      } else {
+        usersMap.set(userId, [userDoc.id]);
+      }
+    });
+
+    const deletionPromises = [];
+    usersMap.forEach((docIds) => {
+      if (docIds.length > 1) {
+        docIds.slice(1).forEach((docId) => {
+          const deletionPromise = usersDb.doc(docId).delete();
+          deletionPromises.push(deletionPromise);
+        });
+      }
+    });
+
+    await Promise.all(deletionPromises);
+    console.log('Duplicate users removed successfully.');
+  } catch (error) {
+    console.log('Error removing duplicate users:', error);
+  }
+}
+
+async function startSearch(userId){
+  bot.sendMessage(userId, 'Начинаю поиск...');
+  userQueue.add(userId);
+}
+
+async function stopSearchOrDialog(userId) {
+  let isDialog = false;
+  chatList.forEach(element1 => {
+    element1.forEach((element2, index) => {
+      if (element2.id === userId) {
+        element1[index] = undefined; // Оновлення елемента в масиві
+        isDialog = true;
+        bot.sendMessage(userId, "Вы покинули диалог");
+      }
+    });
+  });
+  if (!isDialog) {
+    userQueue.exit(userId);
+    bot.sendMessage(userId, "Вы покинули поиск");
+  }
+}
+
+
+// основаня функция запуска
+
 async function run() {
   if (userQueue.checkIfCouldBeInitialized()) {
     let listOfPeople = userQueue.find();
@@ -136,3 +231,5 @@ async function run() {
     });
   }
 }
+
+removeDuplicateUsers();
